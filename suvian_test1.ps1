12914f30708590ba8eb47810d223a0eb07a68fc5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-05-11 21:28:34 -0400

    intermediate revision - added safety

--- suvian_test1.ps1
+++ suvian_test1.ps1
@@ -21,16 +21,37 @@
 function highlight {
 param (
   [System.Management.Automation.PSReference]$window_ref,
+  [System.Management.Automation.PSReference]$document_element_ref,
   [String]$locator,
   [int]$delay = 100
 )
   $window = $window_ref.Value
+  if ($document_element_ref -ne $null) {
+    $document_element = $document_element_ref.Value
+    $element = $null
+    try {
+      $element = $document_element.querySelector($locator)
+      $element.innerHTML | out-null
+    } catch [Exception] {
+      write-Debug ( 'Exception : ' + $_.Exception.Message)
+      return
+    }
+    if ($element -eq $null) {
+      write-Debug (' unable to find {0}' -f $locator )
+      return    
+    }
+  }
   $highlightBorderScript = (@"
 var selector = '{0}';
 var elements = document.querySelectorAll(selector);
 elements[0].style.border='3px solid yellow';
 "@  -f $locator)
-  $window.execScript($highlightBorderScript, 'javascript')
+  try {
+    $window.execScript($highlightBorderScript, 'javascript')
+  } catch [Exception] {
+    write-Debug ( 'Exception : ' + $_.Exception.Message)
+    return
+  }
   start-sleep -milliseconds $delay
 
   $removeBorderScript = (@"
@@ -38,7 +59,12 @@ var selector = '{0}';
 var elements = document.querySelectorAll(selector);
 elements[0].style.border='';
 "@  -f $locator)
-  $window.execScript($removeBorderScript, 'javascript')
+  try {
+    $window.execScript($removeBorderScript, 'javascript')
+  } catch [Exception] {
+    write-Debug ( 'Exception : ' + $_.Exception.Message)
+    return
+  }
 }
 
 function click {
@@ -47,7 +73,7 @@ param (
   [String]$locator
 )
   $window = $window_ref.Value
-  $clickScript = (@"  
+  $clickScript = (@"
 var selector = '{0}';
 var elements = document.querySelectorAll(selector);
 elements[0].click();
@@ -67,29 +93,31 @@ while (($ie.Busy -eq $true ) -or ($ie.ReadyState -ne 4)) { # 4 a.k.a. READYSTATE
   start-sleep -milliseconds 100
 }
 $debug =  $false
-$documentElement = $ie.document.documentElement
+$document_element = $ie.document.documentElement
 $document = $ie.document
 $window = $document.parentWindow
 
-$m1 = $documentElement.getElementsByClassName('intro-message')
+$m1 = $document_element.getElementsByClassName('intro-message')
 $e1 = $m1[0]
 $m2 = $e1.getElementsByTagName('h3')
 $e2 = $m2 | where-object { $_.innerText -match '.*Click Here.*' }
-$e2.FireEvent('onclick', $null)
-# True
-# no effect
+$e2.FireEvent('onclick', $null) ;
+# has no effect
 $e1.click()
+# has no effect
 
-# no effect
-# $documentElement.FireEvent('onclick', $e2)
+# $document_element.FireEvent('onclick', $e2)
 # No such interface supported
 
 $locator = 'body > div.intro-header > div > div > div > div > h3:nth-child(2) > a'
-highlight -locator $locator -window_ref ([ref]$window)
+highlight -locator $locator -window_ref ([ref]$window) -document_element_ref ([ref] $document_element)
 
+# failing test
+$locator = 'body > div.intro-header > div > div > div > div > h3:nth-child(2) > b'
+highlight -locator $locator -window_ref ([ref]$window) -document_element_ref ([ref] $document_element)
 
+$locator = 'body > div.intro-header > div > div > div > div > h3:nth-child(2) > a'
 click -locator $locator -window_ref ([ref]$window)
-
 
 start-sleep -milliseconds 10000
 
