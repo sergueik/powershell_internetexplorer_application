6dad608aadb8aa75bc791f8a8d49e8c2a918215b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-11-12 01:47:08 -0500

    cleaned up exercise

--- parse_local_data.ps1
+++ parse_local_data.ps1
@@ -18,90 +18,37 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
+
+# was replied to the question http://www.cyberforum.ru/powershell/thread2343207.html
+
+# load dirty tables file provided by customer
 $datafile = 'data.html'
 $uri = ('file:///{0}' -f ((resolve-path $datafile).path -replace '\\', '/'))
-# based on http://www.cyberforum.ru/powershell/thread2343207.html
 [Microsoft.PowerShell.Commands.WebResponseObject]$obj = (Invoke-WebRequest -Uri $uri)
-# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-6 
-
 # https://stackoverflow.com/questions/24977233/parse-local-html-file
-# Can't use Poweshell invoke-webrequest cmdlet to *parse* HTML from local files, will only have
-#   StatusCode
-#   StatusDescription
-#   Content
-#   RawContent
-#   Headers
-#   RawContentLength
-# methods available
+# one can't use Poweshell `invoke-webrequest` cmdlet to DOM parsing of local HTML files
+# only methods available: StatusCode, StatusDescription, Content, RawContent, Headers, RawContentLength
 
 $html = new-object -ComObject 'HTMLFile'
-$html.IHTMLDocument2_write($obj.RawContent)
+$html.IHTMLDocument2_write($obj.rawContent)
+$table = $html.documentElement.getElementsByTagName('table').item(0)
 
-# NOTE: can also load like just a file
-# $raw_data = get-content -path (resolve-path $datafile)  
-# $doc = $html.IHTMLDocument2_write($raw_data)
-
-$document = $html.documentElement
-
-$tables = $document.getElementsByTagName('table')
-write-output ('{0} tables' -f $tables.length )
-$table = $document.getElementsByTagName('table').item(0)
-
-try {
-  # will crash even with try / catch
-  # $table.querySelectorAll('tr td')
-  $element = $table.querySelector('tr td:first-of-type') # Exception : Invalid argument.
-  $element.innerHTML
-} catch [Exception]{ 
-  write-output ( 'Exception : ' + $_.Exception.Message)
-}
-
-<#
-$rows = $table.getElementsByTagName('tr')
-write-output ('{0} rows' -f $rows.length )
-# $row = $rows.item(0)
-#  $row | get-member
-@(0..$rows.length) | foreach-object { 
-  $index = $_
-  # write-output $index
-  $row = $table.getElementsByTagName('tr').item($index)
-  # write-output $row.innerHTML
-  try {
-    $cols = $row.getElementsByTagName('TD')
-    write-output ('{0} columns' -f $cols.length )
-    if ($cols.length -eq 9) {
-      write-output ('{0} column: {1}' -f '0', $cols.item(0).innerHTML )
-    }
-  } catch [Exception]{ 
-    write-output ( 'Exception : ' + $_.Exception.Message)
-  }
-}
-#>
-# $table  | get
-# $html.IHTMLDocument2_write($table[0].outerHTML)
-# https://msdn.microsoft.com/en-us/windows/cc304115(v=vs.71)
-write-output ( 'while rows: {0}' -f $table.querySelectorall('tr[bgcolor]').length)
+write-debug 'select rows by background color'
 $length = $table.querySelectorall('tr[bgcolor="#f0f0f0"]').length
+write-debug ( 'white rows: {0}' -f  $length)
 
-@(0..$length) | foreach-object { 
-  $index = $_ 
+@(0..$length) | foreach-object {
+  $index = $_
+  # loading sets into variables appears to be less reliable that re-issuing a COM object call
   $row = $table.querySelectorall('tr[bgcolor="#f0f0f0"]').item($index)
   try {
-  
+    write-debug 'select rows which have email'
     $email = $row.getElementsByTagName('td').item(6)
-    <#
-    if ($email.querySelectorAll('a.external').length -ne 0 ){
-      # Exception : Method invocation failed because [System.__ComObject] does not contain a method named 'querySelectorall'.
-      write-output $email.innerHTML
-      write-output $row.getElementsByTagName('TD')[0].outerHTML
-    }
-    #>
     if ($email.innerText -match '[a-z0-9_]+@mail.ru' ){
-      # Exception : Method invocation failed because [System.__ComObject] does not contain a method named 'querySelectorall'.
       write-output $email.innerText
-      write-output $row.getElementsByTagName('TD').item(0).innerText
+      write-output $row.getElementsByTagName('td').item(0).innerText
     }
-  } catch [Exception]{ 
-    write-output ( 'Exception : ' + $_.Exception.Message)
+  } catch [Exception]{
+    write-debug ('Exception: ' + $_.Exception.Message)
   }
 }
