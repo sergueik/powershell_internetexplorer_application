eba3529700bda7c81fc7f989336f837fe4521938
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-11-15 00:20:06 -0500

    intermediate revision - added integrated test illustraring the difference

--- one_req_webserver.ps1
+++ one_req_webserver.ps1
@@ -1,7 +1,29 @@
+#Copyright (c) 2018 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
 param(
- $html_file = $null,
- [switch]$debug
+  $datafile = 'data.html',
+  $port = '10001',
+  [switch]$debug
 )
+
 function Get-ScriptDirectory
 {
   [string]$scriptDirectory = $null
@@ -42,97 +64,62 @@ function Get-ScriptDirectory
 
 # TODO: better  handle output https://stackoverflow.com/questions/11973775/powershell-get-output-from-receive-job
 # NOTE: start-job argument passing appears to be leading whitespace-sensitive
-Start-Job -scriptblock {param( $port = $null, $webroot = $null, $debug = $false)
-
-  # NOTE: not useful in the job context, stay for one commit only
-  function Get-ScriptDirectory
-  {
-    [string]$scriptDirectory = $null
-
-    if ($host.Version.Major -gt 2) {
-      $scriptDirectory = (Get-Variable PSScriptRoot).Value
-      Write-Debug ('$PSScriptRoot: {0}' -f $scriptDirectory)
-      if ($scriptDirectory -ne $null) {
-        return $scriptDirectory;
-      }
-      $scriptDirectory = [System.IO.Path]::GetDirectoryName($MyInvocation.PSCommandPath)
-      Write-Debug ('$MyInvocation.PSCommandPath: {0}' -f $scriptDirectory)
-      if ($scriptDirectory -ne $null) {
-        return $scriptDirectory;
-      }
-
-      $scriptDirectory = Split-Path -Parent $PSCommandPath
-      Write-Debug ('$PSCommandPath: {0}' -f $scriptDirectory)
-      if ($scriptDirectory -ne $null) {
-        return $scriptDirectory;
-      }
-    } else {
-      $scriptDirectory = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-      if ($scriptDirectory -ne $null) {
-        return $scriptDirectory;
-      }
-      $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-      if ($Invocation.PSScriptRoot) {
-        $scriptDirectory = $Invocation.PSScriptRoot
-      } elseif ($Invocation.MyCommand.Path) {
-        $scriptDirectory = Split-Path $Invocation.MyCommand.Path
-      } else {
-        $scriptDirectory = $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf('\'))
-      }
-      return $scriptDirectory
-    }
-  }
+Start-Job  -ArgumentList @($port, (Get-ScriptDirectory), 'true') -scriptblock {param( $port = $null, $webroot = $null, $debug = $false)
 
   if ($webroot -ne $null) {
     $logpath =  $webroot
   } else {
     $logpath =  $env:TEMP
   }
-  # does not get inherited from caller
-  if  ($debugpreference -eq 'continue') {
-    $debug = $true
-  }
+
+  $logfile = "${logpath}\zz.txt"
+  $drivename = 'MyPowerShellSite'
+
   # based on: https://4sysops.com/archives/building-a-web-server-with-powershell/
+  # See also a similar gist shttps://gist.github.com/19WAS85/5424431
+  # See also https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/creating-powershell-web-server
   if  ($debug) {
-    write-output "Param `$port is ${port}" | out-file "${logpath}\zz.txt" -append
-    write-output ("Get-ScriptDirectory output is {0}" -f (Get-ScriptDirectory)) | out-file "${logpath}\zz.txt" -append
-    write-output "Param `$webroot is `"${webroot}`"" | out-file "${logpath}\zz.txt" -append
+    write-output "Param `$port = ${port}" | out-file $logfile -append
+    write-output "Param `$webroot = `"${webroot}`"" | out-file $logfile -append
   }
-
-  try {
-    add-type -path 'c:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Web.dll'
-  } catch [Exception]{
-    # add-type : Could not load file or assembly 'file:///C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Web.dll' or one of its dependencies. An attempt was made to load a program with an incorrect format.
-    # https://social.technet.microsoft.com/Forums/windowsserver/en-US/44f54abe-8e4c-47fe-a553-568b855a19cb/error-trying-to-load-systemwebdll?forum=winserverpowershell
-  }
-
-  # Unable to find type [System.Web.MimeMapping]
   [System.Reflection.Assembly]::LoadWithPartialName('System.Web') | out-null
 
-  # Exception calling "Start" with "0" argument(s): "Failed to listen on prefix 'http://localhost:18000/' because it conflicts with an existing registration on the machine."
   if ($port -eq $null) {
     $port = Get-Random -minimum 10000 -maximum 20000
   }
   if  ($debug) {
-    write-output ('Runnng on port ' + $port) | out-file "${logpath}\zz.txt" -append
+    write-output ('Runnng on port ' + $port) | out-file $logfile -append
   }
   $listener = New-Object System.Net.HttpListener
   $listener.Prefixes.Add("http://localhost:${port}/")
   $listener.Start() # blocking call
-  $drivename =  'MyPowerShellSite'
+
   if ((get-psdrive -name $drivename -errorAction silentlycontinue) -ne $null) {
     remove-psdrive -Name $drivename
   }
 
-  # New-PSDrive -Name $drivename -PSProvider FileSystem -Root $PWD.Path | out-null
   New-PSDrive -Name $drivename -PSProvider FileSystem -Root $webroot | out-null
-  $Context = $listener.GetContext()
-  $URL = $Context.Request.Url.LocalPath
-  $Content = Get-Content -Encoding Byte -Path "MyPowerShellSite:$URL"
+  $context = $listener.GetContext()
+  $localfile_path = "${drivename}:$($context.Request.Url.LocalPath)"
+  $content = Get-Content -Encoding Byte -Path $localfile_path
 
-  $Context.Response.ContentType = [System.Web.MimeMapping]::GetMimeMapping("MyPowerShellSite:$URL")
-  write-debug $Context.Response.ContentType
-  $Context.Response.OutputStream.Write($Content, 0, $Content.Length)
-  $Context.Response.Close()
+  $context.Response.ContentType = [System.Web.MimeMapping]::GetMimeMapping($localfile_path)
+  # write-debug $context.Response.ContentType
+  $context.Response.OutputStream.Write($content, 0, $content.Length)
+  $context.Response.OutputStream.Flush()
+  start-sleep -millisecond 1000
+  $context.Response.Close()
   $listener.Stop()
-} -ArgumentList @('10001', (Get-ScriptDirectory), 'true')
+  remove-psdrive -Name $drivename
+}
+
+
+$uri = ('file:///{0}' -f ((resolve-path $datafile).path -replace '\\', '/'))
+write-host -foreground 'Green' ('Reading {0}' -f $uri )
+[Microsoft.PowerShell.Commands.WebResponseObject]$obj = (Invoke-WebRequest -Uri $uri)
+$obj| select-object -property 'BaseResponse','StatusCode','StatusDescription','Headers','RawContentStream' | format-list
+
+$uri = "http://localhost:${port}/${datafile}"
+write-host -foreground 'Green' ('Reading {0}' -f $uri )
+[Microsoft.PowerShell.Commands.WebResponseObject]$obj = (Invoke-WebRequest -Uri $uri)
+$obj| select-object -property  'BaseResponse','StatusCode','StatusDescription','Headers','RawContentStream','ParsedHtml','Links' | format-list
