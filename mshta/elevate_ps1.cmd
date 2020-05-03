26dafa7de569e636a3040a008abcf6587d927a61
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-05-03 16:17:25 -0400

    initial snapshot

---
+++ mshta/elevate_ps1.cmd
@@ -0,0 +1,65 @@
+@echo OFF
+@REM origin: https://github.com/ildar-shaimordanov/cmd.scripts/blob/master/unix-like/sudo-ps.bat
+
+::NAME
+::
+::    sudo - execute a command with the elevated privileges
+::
+::SYNOPSIS
+::
+::    sudo
+::    sudo COMMAND [OPTIONS]
+::
+::DESCRIPTION
+::
+::sudo allows to run a command with optional arguments with the elevated
+::privileges. By default, if no any options specified, it runs the
+::command interpreter. The command can be any of binary executables,
+::batch scripts or documents supposed to be open.
+::
+::REQUIREMENTS
+::
+::The script requires PowerShell to be installed on the system.
+@echo off
+
+if /i "%~1" == "/?" goto :print_usage
+if /i "%~1" == "/h" goto :print_usage
+
+call :check_reqs || goto :EOF
+
+setlocal
+
+set "SUDO_HIDDEN="
+
+if "%~1" == "" (
+	call :sudo /s /k cd /d "%CD%"
+	goto :EOF
+)
+
+for %%x in ( .bat .cmd ) do if /i "%~x1" == "%%x" (
+	call :sudo /s /k cd /d "%CD%" "&&" %*
+	goto :EOF
+)
+
+set "SUDO_HIDDEN=-WindowStyle Hidden"
+call :sudo /c start /d "%CD%" %*
+
+goto :EOF
+
+
+:sudo
+endlocal && powershell -Command Start-Process "cmd.exe" -Args '%*' %SUDO_HIDDEN% -Verb RunAs
+goto :EOF
+
+
+:print_usage
+for /f "tokens=* delims=:" %%s in ( 'findstr "^::" "%~f0"' ) do echo:%%s
+goto :EOF
+
+
+:check_reqs
+for %%p in ( powershell.exe ) do if "%%~$PATH:p" == "" (
+	echo:%%~p is required>&2
+	exit /b 1
+)
+exit /b 0
