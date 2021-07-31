df2753875b531c9ee8563afe70ca0679b947577a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-07-31 11:28:30 -0400

    intermediate revision - added passing the argument into caller body as string command argument example

---
+++ mshta/chimera_example3.cmd
@@ -0,0 +1,23 @@
+@echo off
+REM inspired by https://qna.habr.com/q/1027240
+set C=%~nx0
+if NOT "%DEBUG%" equ "" echo Running with DEBUG set
+REM using two envieonment parameters: CALLER and DEBUG
+REM NOTE:  passing arguments appears tricky when
+REM powershell run with command built inline as string
+@powershell.exe -ExecutionPolicy Bypass -Command "$debug=$env:DEBUG;$s=(get-content \"%~f0\") -join \"`n\"; $s = $s.substring($s.IndexOf(\"goto :\"+\"EOF\")+9);if ($debug -ne $null){write-output (\"Running:`n{0}\" -f$s);} invoke-expression -command $s"
+@goto :EOF
+# powershell code
+
+start-sleep 5
+write-output ( 'Calling script: {0}' -f $env:C)
+pause
+
+exit 0
+<#
+# a compact version of this script is:
+@echo off
+@powershell.exe -ExecutionPolicy Bypass -Command "$_=((Get-Content \"%~f0\") -join \"`n\");iex $_.Substring($_.IndexOf(\"goto :\"+\"EOF\")+9)"
+@goto :EOF
+# powershell code
+#>
