00b44293163aa3b894d95bf518afb5502c6d2f21
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-11-14 06:17:24 -0500

    added quirks to address HTMLFile COM Object the MSHTML engine used by HTMLFile is quirks-mode IE5-compatible

--- chm_process.ps1
+++ chm_process.ps1
@@ -1,23 +1,40 @@
+function process_file{
+  param(
+    [string]$filename = 'ch09s33.html'
+  )
+  $rawcontent = Get-Content -Path $filename -Raw
+  $rawcontent = (($rawcontent -join '') - replace '\n','' ) -replace '<head>.*</head>' ,''
 
-param(
-  [string]$filename = 'ch09s33.html'
-)
-$elementawcontent = Get-Content -Path $filename -Raw
+  $htmlfile = New-Object -ComObject 'HTMLFile'
+  $htmlfile.IHTMLDocument2_write($rawcontent )
+  # write-output $htmlfile.title
+  $table = $htmlfile.documentElement.getElementsByTagName('table').item(0)
+  # write-output $table.innerHTML
+  $element = $table.querySelectorall('CODE[class=computeroutput]').item(0)
+  # write-output $element.innerHTML
+  $selector = 'span[class="bold"]'
+  try {
+    $length = $table.querySelectorall($selector).length
+  } catch [Exception] {
+    $length = 0
+  }	
+  if ($length -eq 0 ){
+    return
+  }
+  @(0..$length) | foreach-object {
+    $index = $_
+    # pick 1 column from 3 column row
+    if (($index % 3 ) -eq 0) {
 
-$htmlfile = New-Object -ComObject 'HTMLFile'
-$htmlfile.IHTMLDocument2_write($elementawcontent )
-# write-output $htmlfile.title
-$table = $htmlfile.documentElement.getElementsByTagName('table').item(0)
-# write-output $table.innerHTML
-$element = $table.querySelectorall('CODE[class=computeroutput]').item(0)
-# write-output $element.innerHTML
-$selector = 'span[class="bold"]'
-$length = $table.querySelectorall($selector).length
-@(0..$length) | foreach-object {
-  $index = $_
+    $element  = $table.query Selectorall($selector).item($index)
+    write-output $element.innerText
+  }
+}
 
-  $element = $table.querySelectorall($selector).item($index)
-  write-output $element.innerText
+get-childitem -filter '*html' | foreach-object {
+  $filename = $_
+  write-output ('filename: {0}' -f $filename )
+  process_file -filename $filename
 }
 <#
 Code
