75fa1488fbbc4cbbde2961c8f0ddaf38768e2390
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-11-19 14:14:39 -0500

    intermediate revision - added code skeletons for First, or Last qualifying top-level node  found by combination of getDocumentsByTagName,querySelectorAll and examnation of innerText

--- chm_process.ps1
+++ chm_process.ps1
@@ -5,7 +5,7 @@ function process_file{
   $rawcontent = Get-Content -Path $filename -Raw
   $rawcontent = (($rawcontent -join '') - replace '\n','' ) -replace '<head>.*</head>' ,''
 
-  $htmlfile = New-Object -ComObject 'HTMLFile'
+  $htmlfile = New-object -ComObject 'HTMLFile'
   $htmlfile.IHTMLDocument2_write($rawcontent )
   # write-output $htmlfile.title
   $table = $htmlfile.documentElement.getElementsByTagName('table').item(0)
@@ -42,3 +42,25 @@ Device
 Notes
 
 #>
+$pattern = '(foo|bar)'
+$tag = 'table'
+$selector =  'THEAD TR[class="row"]'
+# function-less flows
+# irst qualifying top-level node
+$firstNode = $dom.getElementsByTagName($tag) |
+    where-object {
+        $_.querySelectorAll($selector) |
+            where-object { $_.InnerText -match $pattern } |
+            select-object -first 1
+    } |
+    select-object -first 1
+# Last qualifying top-level node
+$lastNode = $dom.getElementsByTagName($tag) | 
+    where-object {
+        $_.querySelectorAll($selector) |
+            where-object { $_.InnerText -match $pattern } |
+            select-object -first 1
+    } |
+    select-object -last 1
+
+
